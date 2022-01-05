faf74bd398efbaf958ff975749ea20c9f7a3cd4a
youngjin <youngjin@everdoubling.ai>
2022-01-05 13:34:06 +0900

    pretrain working

--- dataset.py
+++ dataset.py
@@ -1,6 +1,12 @@
 import torch
 from torch.utils.data import Dataset
 import tensorflow.compat.v2 as tf
+
+# prevent tensorflow from pre-allocating memory
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
 from tokenizer import ByT5KoreanTokenizer
 tokenizer = ByT5KoreanTokenizer()
 # from transformers import AutoTokenizer
@@ -9,7 +15,10 @@ tokenizer = ByT5KoreanTokenizer()
 from t5.preprocessors import random_spans_helper, random_spans_noise_mask
 from preprocessors import noise_span_to_unique_sentinel, nonnoise_span_to_unique_sentinel # customized
 
+import glob
+import gzip
 import json
+from tqdm import tqdm
 
 # parameters
 inputs_length = 1024
@@ -32,12 +41,24 @@ tokens_length, targets_length = random_spans_helper(inputs_length, noise_density
 # input_ids, labels = random_span_masking(tf.constant(ids), noise_density, [(2, 3), (4, 5)], 259, mean_noise_span_length)
 
 # dataset
-c4_ko = []
-with open('/data/shared/c4/c4/multilingual/c4-ko.tfrecord-00000-of-01024.json') as f:
-    for line in f:
-        c4_ko.append(json.loads(line))
-        # c4_ko.append(tokenizer(json.loads(line)['text'], add_special_tokens=False).input_ids)
+# c4_ko = []
+c4_ko_train = []
+c4_ko_eval = []
 
+# with open('/data/shared/c4/c4/multilingual/c4-ko.tfrecord-00000-of-01024.json') as f:
+#     for line in f:
+#         c4_ko.append(json.loads(line))
+#         # c4_ko.append(tokenizer(json.loads(line)['text'], add_special_tokens=False).input_ids)
+
+n_texts = 0
+for filename in tqdm(glob.glob("/data/shared/c4/c4/multilingual/*ko*.gz")[:500]):
+    with gzip.open(filename) as f:
+        for line in f:
+            n_texts += 1
+            if n_texts % 100 != 0 or len(c4_ko_eval) >= 1000:
+                c4_ko_train.append(json.loads(line))
+            else:
+                c4_ko_eval.append(json.loads(line))
 # from Korpora import Korpora
 # corpus = Korpora.load("kowikitext")
 # ids = tokenizer(corpus.train.texts, padding=True, truncation=True, max_length=512).input_ids
@@ -46,21 +67,25 @@ with open('/data/shared/c4/c4/multilingual/c4-ko.tfrecord-00000-of-01024.json') 
 
 class KoreanDataset(Dataset):
     def __init__(self, evaluate: bool = False):
-        # self.examples = ids_train if not evaluate else ids_eval
+        self.examples = c4_ko_train if not evaluate else c4_ko_eval
         return
 
     def __len__(self):
-        return len(c4_ko)
+        return len(self.examples)
 
     def __getitem__(self, i):
-        ids = tokenizer(c4_ko[i]['text'], padding=True, truncation=True, max_length=tokens_length, add_special_tokens=False).input_ids
-        input_ids, labels = random_span_masking(tf.constant(ids), noise_density, [(2, 3), (4, 5)], 259, mean_noise_span_length)
+        ids = tokenizer(self.examples[i]['text'], padding=True, truncation=True, max_length=tokens_length, add_special_tokens=False).input_ids
+        input_ids, labels = random_span_masking(tf.constant(ids), noise_density, [(i, i), (i, i)], 259, mean_noise_span_length)
+        input_ids = add_eos(input_ids)
+        labels = add_eos(labels)
 
         return { 'input_ids': torch.tensor(input_ids.numpy()), 'labels': torch.tensor(labels.numpy()) }
         # return { 'input_ids': torch.tensor(self.examples[i]), 'decoder_input_ids': torch.tensor(self.examples[i]), 'label_ids': torch.tensor(self.examples[i]) }
         # return { 'input_ids': torch.tensor(self.examples[i]), 'label_ids': torch.tensor([0]) }
 
 if __name__ == "__main__":
-    ds = KoreanDataset()
-    print(ds[0])
+    ds_train = KoreanDataset(evaluate=False)
+    ds_eval = KoreanDataset(evaluate=True)
+    print(ds_train[0])
+    print(ds_eval[0])
     print('Done.')
--- pretrain.py
+++ pretrain.py
@@ -1,9 +1,13 @@
 
-from transformers import T5Config, T5Model, T5ForConditionalGeneration, AutoTokenizer, DataCollatorForSeq2Seq, TrainingArguments, Trainer
+import torch
+
+from transformers import T5Config, T5Model, T5ForConditionalGeneration, T5Tokenizer, AutoTokenizer, DataCollatorForSeq2Seq, TrainingArguments, Trainer
 from tokenizer import ByT5KoreanTokenizer
 import dataset
 
-model_path = './byT5-Korean-small'
+# import datetime
+# model_path = './byT5-Korean-small-{}'.format(datetime.datetime.now())
+model_path = './byT5-Korean-small-5'
 
 model_config_small = {
     "_name_or_path": "google/byt5-small",
@@ -64,7 +68,7 @@ model_config_large = {
     "vocab_size": 384
 }
 
-def main():
+def train():
     model_config = T5Config(**model_config_small)
     # model = T5Model(config=model_config) # 그냥 T5 모델로 하면 안되고 T5ForConditionalGeneration를 사용해야 함
     model = T5ForConditionalGeneration(config=model_config)
@@ -73,14 +77,16 @@ def main():
         output_dir=model_path,          # output directory to where save model checkpoint
         evaluation_strategy="steps",    # evaluate each `logging_steps` steps
         overwrite_output_dir=True,      
-        num_train_epochs=1000,            # number of training epochs, feel free to tweak
-        per_device_train_batch_size=32, # the training batch size, put it as high as your GPU memory fits
-        gradient_accumulation_steps=2,  # accumulating the gradients before updating the weights
-        per_device_eval_batch_size=64,  # evaluation batch size
-        logging_steps=500,              # evaluate, log and save model checkpoints every 1000 step
-        save_steps=500,
+        num_train_epochs=1,             # number of training epochs, feel free to tweak
+        per_device_train_batch_size=8,  # the training batch size, put it as high as your GPU memory fits
+        gradient_accumulation_steps=4,  # accumulating the gradients before updating the weights
+        per_device_eval_batch_size=8,   # evaluation batch size
+        logging_steps=1000,              # evaluate, log and save model checkpoints every 1000 step
+        save_steps=1000,
         load_best_model_at_end=True,    # whether to load the best model (in terms of loss) at the end of training
         save_total_limit=3,             # whether you don't have much space so you let only 3 model weights saved in the disk
+        ignore_data_skip=True,
+        # dataloader_num_workers=1,
     )
 
     trainer = Trainer(
@@ -93,21 +99,33 @@ def main():
     )
 
     trainer.train()
+    # trainer.train('byT5-Korean-small-2/checkpoint-4500') # continue training
 
+def test_base():
+    tokenizer = T5Tokenizer.from_pretrained('t5-small')
+    model = T5ForConditionalGeneration.from_pretrained('t5-small')
+    
+    input_ids = tokenizer("summarize: studies have shown that owning a dog is good for you", return_tensors="pt").input_ids  # Batch size 1
+    outputs = model.generate(input_ids)
+    print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+    
 def test():
     tokenizer = ByT5KoreanTokenizer()
     # model = T5ForConditionalGeneration.from_pretrained('t5-small')
-    model = T5ForConditionalGeneration.from_pretrained(model_path + '/checkpoint-1000')
-
-    input_ids = tokenizer('The <extra_id_0> walks in <extra_id_1> park', return_tensors='pt').input_ids
-    labels = tokenizer('<extra_id_0> cute dog <extra_id_1> the <extra_id_2>', return_tensors='pt').input_ids
-    outputs = model(input_ids=input_ids, labels=labels)
-    loss = outputs.loss
-    logits = outputs.logits
-
-    output_ids = model.generate(input_ids)
+    model = T5ForConditionalGeneration.from_pretrained(model_path + '/checkpoint-4500')
+    ds_train = dataset.KoreanDataset(evaluate=False)
+    for i in range(10):
+        # input_ids = tokenizer('The <extra_id_0> walks in <extra_id_1> park', return_tensors='pt').input_ids
+        # labels = tokenizer('<extra_id_0> cute dog <extra_id_1> the <extra_id_2>', return_tensors='pt').input_ids
+        input_ids = torch.unsqueeze(ds_train[i]['input_ids'], 0).type(torch.long)
+        labels = torch.unsqueeze(ds_train[i]['labels'], 0).type(torch.long)
+        outputs = model(input_ids=input_ids, labels=labels)
+        loss = outputs.loss
+        logits = outputs.logits
+        output_ids = model.generate(input_ids)[0][1:] # ignore output_ids[0] which is decoder_start_token_id
+        print(tokenizer.decode(output_ids))
 
 if __name__ == "__main__":
-    main()
+    train()
     # test()
     print("Done")
